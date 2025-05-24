812da1e8162d4d6814893ca2973100e1788b5756
Siddharth Pratap Singh <112649371+Sid-istic@users.noreply.github.com>
2025-05-24 06:18:55 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+
 
 with open("app/best_model.pkl", "rb") as f:
     model = pickle.load(f)
